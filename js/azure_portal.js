ce9845165970671e4946062b8c597e45082ec9de
tyamasaki <tyamasaki@fortinet.com>
2018-07-19 10:18:02 +0900

    Init_03

--- js/azure_portal.js
+++ js/azure_portal.js
@@ -2,32 +2,37 @@
 
 // Font Awesome
 $(function() {
-
     _sshConnectIdWather();
-    
 });
 
 
 
 // SSH Connection ID Wather
 function _sshConnectIdWather(){
+	var panelId = '#fxsContextPaneTitle';
 
-	var sshId = '#fxsContextPaneTitle';
-    if ($(sshId).length) {
-        if ($(sshId).text('仮想マシンに接続する')) {
-            $(sshId).closest('section').parent().children('.fxc-copyablelabel-textbox').children('input').css('background-color','red');
-            $(sshId).closest('section').parent().children("div:has(.fxc-copyablelabel-textbox)").css('background-color','red');
-            $(sshId).closest('section').parent().css('background-color','red');
-            var sshValue = $(sshId).closest('section').parent().children('.fxc-copyablelabel-textbox').children('input').attr("title");
-            console.log(sshValue);
+    if (!$(panelId).next().children('i').hasClass("custom_new_link")) {
+        if ($(panelId).length) {
+            if ($(panelId).text('仮想マシンに接続する') || $(panelId).text('Connect to virtual machine')) {
+                var sshValue = $(panelId).closest('section').find('.fxc-copyablelabel-textbox').find('input').attr("title");
+                var newLink = _sshLinkCreater(sshValue);
+
+                if (newLink.slice(0, 4) == "ssh:") {
+                    var linkHtml = '<a href="' + newLink + '" target="_blank"><i class="fa fa-external-link custom_new_link" aria-hidden="true" style="padding:0 8px 0px; font-size:24px;"></i></a>';
+                    $(panelId).after(linkHtml);	
+                }
+            }
         }
-    
     }
-
 	setTimeout(_sshConnectIdWather, 2000);
-
 }
 
+function _sshLinkCreater(value){
+    if (value.slice(0, 3) == "ssh") {
+        var result = 'ssh://' + value.slice(3).trim();
+        return result;
+    }
+}
 
 
 
